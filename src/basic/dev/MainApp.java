45ef0755880c6bfbb8eccd57d7859c4dc5239495
phuongthanh9502 <navynguyen9502@gmail.com>
2021-04-23 20:52:26 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,47 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		        //Tao Thong Tin student
+				String name;
+				System.out.println("nhập tên học sinh");
+				String id;
+				System.out.println("nhập mã sinh viên");
+				String cmnd;
+				String nameSchool;
+				float dtb;
+				
+				// Tao Thong Tin Cong nhân
+				String nameCongnhan;
+				String diaChi;
+				String soĐienThoai;
+				float heSoLương;
+				
+				// Tao Thong tin car
+				String namecar;
+				String hãng;
+				String giá;
+				String màu;
+				
+				name = " Nguyen Thi Phuong Thanh";
+				id = "2060092";
+				cmnd = " 197371293";
+				nameSchool= " HueIC";
+				dtb = 8.8f;
+				
+				
+				namecar = " Mercedes-Benz";
+				hãng = "Germany";
+				giá = " 4,599,000,000";
+				màu = " đen ";
+				
+				System.out.println("Ho va ten: " + name);
+				System.out.println("ID = " + id);
+				System.out.println("NAMESHOOL:" + nameSchool);
+
+				
+
+	}
+
+}
